72ab87a974105ae68883dcf48d3bea061b009d26
masterUNG <phrombutr@gmail.com>
2015-12-27 10:51:08 +0700

    Adapter

---
+++ app/src/main/java/appewtc/masterung/mytraffic/MyAdapter.java
@@ -0,0 +1,50 @@
+package appewtc.masterung.mytraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by masterUNG on 12/27/15 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private String[] titleStrings, detailStrings;
+    private int[] iconInts;
+
+    public MyAdapter(Context objContext, String[] titleStrings, String[] detailStrings, int[] iconInts) {
+        this.objContext = objContext;
+        this.titleStrings = titleStrings;
+        this.detailStrings = detailStrings;
+        this.iconInts = iconInts;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+
+        return objView1;
+    }
+}   // Main Class
