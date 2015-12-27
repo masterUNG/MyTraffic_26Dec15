e4669a06496ee192bc3f77e54872dd4e78b3aaae
masterUNG <phrombutr@gmail.com>
2015-12-27 11:57:15 +0700

    ก่อนออกแบบ UI เอง

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -27,7 +27,53 @@ public class MainActivity extends AppCompatActivity {
         //Button Controller
         buttonController();
 
+        //ListView Contorller
+        listViewController();
+
     }   // Main Method
+
+    private void listViewController() {
+
+        //#1
+        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
+                R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
+                R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
+                R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
+
+        //#2
+        String[] strTitle = new String[20];
+        strTitle[0] = "หัวข้อทีี่ 1";
+        strTitle[1] = "หัวข้อทีี่ 2";
+        strTitle[2] = "หัวข้อทีี่ 3";
+        strTitle[3] = "หัวข้อทีี่ 4";
+        strTitle[4] = "หัวข้อทีี่ 5";
+        strTitle[5] = "หัวข้อทีี่ 6";
+        strTitle[6] = "หัวข้อทีี่ 7";
+        strTitle[7] = "หัวข้อทีี่ 8";
+        strTitle[8] = "หัวข้อทีี่ 9";
+        strTitle[9] = "หัวข้อทีี่ 10";
+        strTitle[10] = "หัวข้อทีี่ 11";
+        strTitle[11] = "หัวข้อทีี่ 12";
+        strTitle[12] = "หัวข้อทีี่ 13";
+        strTitle[13] = "หัวข้อทีี่ 14";
+        strTitle[14] = "หัวข้อทีี่ 15";
+        strTitle[15] = "หัวข้อทีี่ 16";
+        strTitle[16] = "หัวข้อทีี่ 17";
+        strTitle[17] = "หัวข้อทีี่ 18";
+        strTitle[18] = "หัวข้อทีี่ 19";
+        strTitle[19] = "หัวข้อทีี่ 20";
+
+        //#3
+        String[] strDetail = getResources().getStringArray(R.array.detail_short);
+
+        //Call MyAdapter
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, strTitle, strDetail, intIcon);
+        trafficListView.setAdapter(objMyAdapter);
+
+    }   // listViewController
 
 
     private void buttonController() {
--- app/src/main/java/appewtc/masterung/mytraffic/MyAdapter.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MyAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by masterUNG on 12/27/15 AD.
@@ -44,6 +46,16 @@ public class MyAdapter extends BaseAdapter{
         LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View objView1 = objLayoutInflater.inflate(R.layout.my_listview, viewGroup, false);
 
+        //For TextView
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[i]);
+
+        TextView detailTextView = (TextView) objView1.findViewById(R.id.textView3);
+        detailTextView.setText(detailStrings[i]);
+
+        //For Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
 
         return objView1;
     }
