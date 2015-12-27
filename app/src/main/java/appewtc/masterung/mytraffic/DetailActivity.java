e9b5f032af8eadbbc5b8b2c466a6e07324bbc4c0
masterUNG <phrombutr@gmail.com>
2015-12-27 14:24:10 +0700

    Show View Finish

--- app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
@@ -2,12 +2,57 @@ package appewtc.masterung.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Initial Widget
+        initilaWidget();
+
+        //Show View
+        showView();
+
+    }   // Main Method
+
+    public void clickBack(View view) {
+        finish();
     }
-}
+
+    private void showView() {
+
+        //Receive Value From Intent
+        String strTitle = getIntent().getStringExtra("Title");
+        int intImage = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+
+        //Show Title
+        titleTextView.setText(strTitle);
+
+        //Show Image
+        trafficImageView.setImageResource(intImage);
+
+        //Show Detail
+        String[] detailStrings = getResources().getStringArray(R.array.detail_long);
+        detailTextView.setText(detailStrings[intIndex]);
+
+
+    }   // showView
+
+    private void initilaWidget() {
+        titleTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+    }
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private void listViewController() {
 
         //#1
-        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+        final int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
                 R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
                 R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
                 R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
 
         //#2
-        String[] strTitle = new String[20];
+        final String[] strTitle = new String[20];
         strTitle[0] = "หัวข้อทีี่ 1";
         strTitle[1] = "หัวข้อทีี่ 2";
         strTitle[2] = "หัวข้อทีี่ 3";
@@ -67,11 +68,30 @@ public class MainActivity extends AppCompatActivity {
         strTitle[19] = "หัวข้อทีี่ 20";
 
         //#3
-        String[] strDetail = getResources().getStringArray(R.array.detail_short);
+        final String[] strDetail = getResources().getStringArray(R.array.detail_short);
 
         //Call MyAdapter
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, strTitle, strDetail, intIcon);
         trafficListView.setAdapter(objMyAdapter);
+
+        //Active When Click on ListView
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Intent to DetailActivity
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+
+                //putExtra
+                objIntent.putExtra("Title", strTitle[i]);
+                objIntent.putExtra("Image", intIcon[i]);
+                objIntent.putExtra("Index", i);
+
+                startActivity(objIntent);
+
+            }   // event
+        });
+
 
     }   // listViewController
 
